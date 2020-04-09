2e74752f6a1b411169217b02acde14c7d85397d7
sapatra <sapatra@teksystems.com>
2020-04-09 12:22:46 +0530

    adding local changes

---
+++ app.js
@@ -0,0 +1,38 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+yargs.parse()
